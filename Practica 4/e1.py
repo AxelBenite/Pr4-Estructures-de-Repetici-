67399a2f0a3928a069476230b29bb8be84741fd6
axel.benitez.7e7 <axel.benitez.7e7@itb.cat>
2023-12-01 12:34:30 +0100

    asdasd

--- Practica 4/e1.py
+++ Practica 4/e1.py
@@ -5,26 +5,31 @@ Oscar Bravo
 M03 - Programacio
 01/12/2023
 """
-numero = input().split()
-longitud = len(numero)
+try:
+    numero = input().split()
+    longitud = len(numero)
 
-contador_0 = 0
-contador_p = 0
-contador_n = 0
+    contador_0 = 0
+    contador_p = 0
+    contador_n = 0
 
-contador_a = 0
-while longitud != 0:
+    contador_a = 0
+    if longitud == 10:
+        while longitud != 0:
 
-    if int(numero[contador_a]) == 0:
-        contador_0 += 1
-    elif int(numero[contador_a]) > 0:
-        contador_p += 1
-    elif int(numero[contador_a]) < 0:
-        contador_n += 1
-    contador_a += 1
-    longitud -= 1
-print(contador_0)
-print(contador_p)
-print(contador_n)
+            if int(numero[contador_a]) == 0:
+                contador_0 += 1
+            elif int(numero[contador_a]) > 0:
+                contador_p += 1
+            elif int(numero[contador_a]) < 0:
+                contador_n += 1
+            contador_a += 1
+            longitud -= 1
+        print(contador_0)
+        print(contador_p)
+        print(contador_n)
 
-
+    else:
+        print("PON 10 numeros que podrian ser 10000000 que podrian ser 0 que podrian ser...")
+except:
+    print("LA CAGASTE BURLANCASTER")
